16ea876859fd13d44856fdb73cfe0882bf1df114
mshoaib112215 <75632120+mshoaib112215@users.noreply.github.com>
2022-07-31 15:16:54 +0500

    name assigned (Fostabay) and backend deployed

--- backend_sanity/schemas/product.js
+++ backend_sanity/schemas/product.js
@@ -2,40 +2,43 @@ export default{
     name: "product",
     title: "Product",
     type: "document",
-    feild:[{
-        name : "image",
-        title: 'Image',
-        type: 'array',
-        of: [{type: "image"}],
-        option:{
-            hotspot: true,
-        }
-    },
-    {
-        name: "name",
-        title: "Name",
-        type: 'string',
+    fields:[
+        {
+            name : "image",
+            title: 'Image',
+            type: 'array',
+            of: [{type: "image"}],
+            options:{
+                hotspot: true,
+            }
+        },
+
+        {
+            name: "name",
+            title: "Name",
+            type: 'string',
+            
+        },
+        {
+            name: 'slug',
+            title:"Slug",
+            type: 'slug',
+            options:{
+                source: 'name',
+                maxLength: 90,
+            }
+        },
+        {
+            name: 'price',
+            title: "Price",
+            type: "number"
+        },
+        {
+            name: "details",
+            title: "Details",
+            type: 'string',
         
-    },
-    {
-        name: 'slug',
-        title:"Slug",
-        type: 'slug',
-        option:{
-            source: 'name',
-            maxLength: 90,
         }
-    },
-    {
-        name: 'price',
-        title: "Price",
-        type: "number"
-    },
-    {
-        name: "details",
-        title: "Details",
-        type: 'string',
-    
-    }]
+    ]
 
 }
