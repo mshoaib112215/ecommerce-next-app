4e819731a18edbede1bad0774bfd67193ac2fad4
mshoaib112215 <75632120+mshoaib112215@users.noreply.github.com>
2022-08-19 22:49:39 +0500

    products section completed

--- components/Products.jsx
+++ components/Products.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { urlFor } from '../lib/client'
+import Link from "next/link"
 
-const Products = () => {
+const Products = ({product :{name, image, slug, price}}) => {
   return (
-    <div>Products</div>
+    <div>
+      <Link href = {`/product/${slug.current}`}>
+
+        <div className="product-card">
+          <img src = {urlFor(image && image[0])} alt= {name}
+            width ={250}
+            height={250}
+            className = "product-image"
+          />
+          <p className = "product-name">{name}</p>
+          <p clasName = "product-price">${price}</p>
+
+        </div>
+
+      </Link>
+    </div>
+
+    
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Home = ({bannerData ,products }) => {
 
       <div className='products-container'>
         {products?.map((product)=>(
-          product.name
+          <Products key = {product._id} product = {product}/>
         ))}
       </div>
       <FooterBanner/>
